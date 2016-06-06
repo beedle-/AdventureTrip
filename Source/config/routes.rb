60f7e487b15c99b4772a675496cadc3eee6d42a8
Bastien Rouiller <bastien.rouiller@gmail.com>
2016-06-06 22:18:23 +0200

    Forgot to push routes.rb

--- Source/config/routes.rb
+++ Source/config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users
   resources :comments
+    resources :permissions do
+    member do
+      get 'accept'
+      get 'refuse'
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
